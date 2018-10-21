c7d25113a3a96988fff7c72afa0b2916faf857b2
AyronKotlet <kotlickimarcin@gmail.com>
2018-10-21 11:20:28 +0200

    Initialized Tests

---
+++ PreyPredatorSim/PreyPredatorSim.Test/Brain/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PreyPredatorSim.Test.Brain
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
