b5a1e64cc858d055d5b18842bd0c9c9ab553b33d
AyronKotlet <kotlickimarcin@gmail.com>
2018-10-21 12:30:43 +0200

    Clamp implemented

--- PreyPredatorSim/PreyPredatorSim.Brain/Extensions/MathExtensions.cs
+++ PreyPredatorSim/PreyPredatorSim.Brain/Extensions/MathExtensions.cs
@@ -1,7 +1,12 @@
 ﻿namespace PreyPredatorSim.Brain.Extensions
 {
-    public class MathExtensions
+    public static class MathExtensions
     {
-        
+        public static double Clamp(this double value, double min = double.MinValue, double max = double.MaxValue)
+        {
+            if (value < min) return min;
+            if (value > max) return max;
+            return value;
+        }
     }
 }
